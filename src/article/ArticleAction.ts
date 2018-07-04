6b295cb5c9b90f7db46ac24fe47ffc98c050bb28
kasakaid <kasakaid@outlook.com>
2018-07-05 02:01:04 +0900

    コメント追加

--- src/article/ArticleAction.ts
+++ src/article/ArticleAction.ts
@@ -5,6 +5,9 @@ import ArticleActionTypes from './ArticleActionTypes'
 export interface IStateRoot {
     articles: Article[], 
 }
+/**
+ * Reducer に dispatch するインターフェース
+ */
 interface IArticleAction extends Action {
     type: ArticleActionTypes;
     payload: Article;
